879d002bddd02c61fe697a19e3bd17c479a2a0db
Sitao Wang <sitaowang1998@outlook.com>
2017-11-06 23:11:43 -0500

    Implemented all the methods in FileManager.java
    Add static final Sting[] IMAGE_EXTENSION to store all the possible
    extension of an image file, add a private helper function IsImage()
     and two public function getDirectoryAbsolutePath() and renewDirect
    ory(), implement the constructor and the getters.

--- phase1/src/Model/FileManager.java
+++ phase1/src/Model/FileManager.java
@@ -1,15 +1,20 @@
 package Model;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class FileManager {
 
-    private String directoryRoute;
+    private File directory;
     private ArrayList<String> imageFileNames;
     private ArrayList<String> directoryNames;
+    private static final String[] IMAGE_EXTENSION= {".png", ".jpg",".img"};
 
-    FileManager(String directoryRoute) {
-        this.directoryRoute = directoryRoute;
+    public FileManager(String directoryRoute) {
+        this.directory = new File(directoryRoute);
+        imageFileNames = new ArrayList<>(0);
+        directoryNames = new ArrayList<>(0);
+
     }
 
     public ArrayList<String> getImageFileNames() {
@@ -18,5 +23,32 @@ public class FileManager {
 
     public ArrayList<String> getDirectoryNames() {
         return directoryNames;
+    }
+
+    public String getDirectoryAbsolutePath() {
+        return directory.getAbsolutePath();
+    }
+
+    public void renewDirectory(){
+        File[] fileList = directory.listFiles();
+        if (fileList != null) {
+            for (File file : fileList) {
+                if (file.isDirectory()) {
+                    directoryNames.add(file.getPath());
+                } else if (isImage(file)) {
+                    imageFileNames.add(file.getPath());
+                }
+
+            }
+        }
+    }
+
+    private boolean isImage(File file) {
+        for (String imageExtension: IMAGE_EXTENSION) {
+            if (file.getPath().endsWith(imageExtension)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
