2806a6c953f0868acd47b3be2543e5f93c1e1695
Sitao Wang <sitaowang1998@outlook.com>
2017-11-06 21:29:50 -0500

    Created stubs for Image.java and Observer.java

---
+++ phase1/src/Model/Image.java
@@ -0,0 +1,27 @@
+package Model;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Image {
+
+    private LogManager logManager;
+    private ArrayList<Observer> observers;
+    private File file;
+
+    public Image(String pathname) {
+
+    }
+
+    public void rename(String newPathname) {
+
+    }
+
+    public void registerObserver(Observer observer) {
+
+    }
+
+    public void deleteObserver(Observer observer) {
+
+    }
+}
