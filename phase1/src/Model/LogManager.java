acccd6c16d35774d26d2d3c8efb8f8cb744cbf2d
Jingyuan Gao <rooney.gao@mail.utoronto.ca>
2017-11-08 02:27:47 -0500

    Added private variables Path logFilePath and ArrayList<String> tagLogStrings; changed the constructor to assign these variables

--- phase1/src/Model/LogManager.java
+++ phase1/src/Model/LogManager.java
@@ -1,22 +1,29 @@
 package Model;
 
 import java.io.File;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.nio.file.*;
+import java.nio.charset.*;
 
 public class LogManager {
 
     private File logFile;
+    private Path logFilePath;
     private ArrayList<TagInfo> tagLogs;
+    private ArrayList<String> tagLogsStrings;
     private ArrayList<Observer> observers;
 
     public LogManager(String pathname) {
         logFile = new File(pathname);
+        logFilePath = logFile.toPath();
+        for (TagInfo element : tagLogs) {
+            tagLogsStrings.add(element.toString());
+        }
     }
 
     private boolean logFileExists() {
-        Path filePath = logFile.toPath();
-        return Files.exists(filePath);
+        return Files.exists(logFilePath);
     }
 
     public void createLogFile() {
