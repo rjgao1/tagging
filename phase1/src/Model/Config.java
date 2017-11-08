e8811495a01c49244cf9392253cfcfdbe940258b
Sitao Wang <sitaowang1998@outlook.com>
2017-11-07 20:24:32 -0500

    Implemented all the methods in Config.java
    Add a new static File variable configFile and a new static String variable defaultDirectory to store the default directory shown everytime user opens a new window. Add a setter getter to defaultDirectory.

--- phase1/src/Model/Config.java
+++ phase1/src/Model/Config.java
@@ -1,24 +1,44 @@
 package Model;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Config {
 
     private static boolean viewTags;
     private static String defaultPath;
+    private static File configFile;
 
     public static boolean hasConfigFile() {
         String dir = System.getProperty("user.dir");
-        File configFile = new File(dir, "config.txt");
+        configFile = new File(dir, "config.txt");
         return configFile.isFile();
     }
 
-    public static void readConfigFile() {
+    public static void readConfigFile() throws IOException{
+        BufferedReader configBR = new BufferedReader(new FileReader(configFile));
 
+        viewTags = Boolean.getBoolean(configBR.readLine());
+        defaultPath = configBR.readLine();
+
+        configBR.close();
     }
 
-    public static void writeConfigFile() {
+    public static void writeConfigFile() throws IOException{
+        FileOutputStream configFOS = new FileOutputStream(configFile);
+        BufferedWriter configBW = new BufferedWriter(new OutputStreamWriter(configFOS));
 
+        configBW.write(String.valueOf(viewTags));
+        configBW.newLine();
+        configBW.write(defaultPath);
+
+        configBW.close();
+        configFOS.close();
     }
 
     public static void setViewTags(boolean viewTags) {
@@ -27,6 +47,10 @@ public class Config {
 
     public static boolean getViewTags() {
         return Config.viewTags;
+    }
+
+    public static void setDefaultPath(String defaultPath) {
+        Config.defaultPath = defaultPath;
     }
 
     public static String getDefaultPath() {
