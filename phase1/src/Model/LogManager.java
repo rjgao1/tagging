365caa9dcfe5ca6be56a91e846f057537e93e169
Jingyuan Gao <rooney.gao@mail.utoronto.ca>
2017-11-14 17:35:32 -0500

    implemented LogManager.registerObserver and LogManager.deleteObserver methods

--- phase1/src/Model/LogManager.java
+++ phase1/src/Model/LogManager.java
@@ -48,11 +48,11 @@ public class LogManager {
     }
 
     public void registerObserver(Observer observer) {
-
+        observers.add(observer);
     }
 
     public void deleteObserver(Observer observer) {
-
+        observers.remove(observer);
     }
 
 
