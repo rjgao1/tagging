0b9e2fde9d243c3e0db54f6729c8dd30deb5ec10
Sitao Wang <sitaowang1998@outlook.com>
2017-11-14 15:54:14 -0500

    Created ConfigPage and stub for its controller

---
+++ phase1/src/Controller/ConfigPageController.java
@@ -0,0 +1,20 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
+
+public class ConfigPageController {
+
+    public void selectDirectory() {
+
+    }
+
+    public void applyButtonClicked() {
+
+    }
+
+    public void cancelButtonClicked() {
+
+    }
+
+}
