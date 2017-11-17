c9d65b7da4fa8c49585e11941be261f459dc4cc1
Sitao Wang <sitaowang1998@outlook.com>
2017-11-16 22:08:18 -0500

    Implemented MessageBoxController

--- phase1/src/Controller/MessageBoxController.java
+++ phase1/src/Controller/MessageBoxController.java
@@ -1,14 +1,32 @@
 package Controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class MessageBoxController {
 
+    private String title;
     @FXML
     private String message;
+    @FXML
+    private Label messageLabel;
+
+    public MessageBoxController(String message, String title) {
+        this.message = message;
+        this.title = title;
+    }
+
+    @FXML
+    public void initialize() {
+        ((Stage) messageLabel.getScene().getWindow()).setTitle(title);
+        ((Stage) messageLabel.getScene().getWindow()).setResizable(false);
+        ((Stage) messageLabel.getScene().getWindow()).initStyle(StageStyle.UNDECORATED);
+    }
 
     public void okButtonClicked(){
-
+        ((Stage) messageLabel.getScene().getWindow()).close();
     }
 
 }
