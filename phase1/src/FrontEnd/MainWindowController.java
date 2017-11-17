c65276103c618cb64a6f31296f891a25a5c2df1e
Sitao Wang <sitaowang1998@outlook.com>
2017-11-17 13:22:37 -0500

    Implemented MainWindowController.chooseFile

--- phase1/src/FrontEnd/MainWindowController.java
+++ phase1/src/FrontEnd/MainWindowController.java
@@ -2,6 +2,7 @@ package FrontEnd;
 
 import Model.Config;
 import Model.FileManager;
+import Model.Image;
 import Model.Tag;
 
 import javafx.collections.FXCollections;
@@ -11,27 +12,34 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
+
 
 public class MainWindowController {
 
 
     private Stage stage;
     private FileManager fileManager;
+    private Image image;
 
     @FXML
-    ListView fileList;
+    ListView<String> fileList;
     @FXML
-    ListView tagList;
+    ListView<String> tagList;
     @FXML
     TextField tagText;
 
     private ObservableList<String> files;
-    private ObservableList<Tag> tags;
+    private ObservableList<String> tags;
 
     public MainWindowController() {
         fileManager = new FileManager(Config.getDefaultPath());
@@ -48,10 +56,16 @@ public class MainWindowController {
         }
         stage.setScene(new Scene(root));
 
-        //Load the data
+        //Load fileList
         ArrayList<String> list = fileManager.getImageFileNames();
         list.addAll(fileManager.getDirectoryNames());
         files = FXCollections.observableArrayList(list);
+        fileList.setItems(files);
+
+        fileList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        tagList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+
+        stage.show();
     }
 
     public void closeWindow() {
@@ -59,11 +73,44 @@ public class MainWindowController {
     }
 
     public void openMainWindow() {
-
+        MainWindowController mainWindowController = new MainWindowController();
     }
 
     public void openDirectory() {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Choose the new directory");
+        File selectedDirectory = directoryChooser.showDialog(stage);
+        fileManager = new FileManager(selectedDirectory.getAbsolutePath());
 
+        // Reload fileList
+        ArrayList<String> list = fileManager.getImageFileNames();
+        list.addAll(fileManager.getDirectoryNames());
+        files = FXCollections.observableArrayList(list);
+    }
+
+    public void chooseFile(MouseEvent e) {
+        File file = new File(fileManager.getDirectoryAbsolutePath() + System.getProperty("file.separator")
+                + fileList.getSelectionModel().getSelectedItem());
+        if (e.getClickCount() == 1) {
+            if (file.isFile()) {
+                image = new Image(file.getAbsolutePath());
+                //Load tagList
+                Tag[] tagArray = Image.getTagsFromName(file.getAbsolutePath());
+                tags = FXCollections.observableArrayList();
+                for (Tag tag : tagArray) {
+                    tags.add(tag.getContent());
+                }
+                tagList.setItems(tags);
+            }
+        } else if(e.getClickCount() == 2) {
+            if (file.isDirectory()) {
+                fileManager = new FileManager(file.getAbsolutePath());
+                // Reload fileList
+                ArrayList<String> list = fileManager.getImageFileNames();
+                list.addAll(fileManager.getDirectoryNames());
+                files = FXCollections.observableArrayList(list);
+            }
+        }
     }
 
     public void addTags() {
