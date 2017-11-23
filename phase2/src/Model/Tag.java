0f2ab6bb6ce27e4df4bc8fc0365c6683379f13fa
APHRODITE <afroditi.rizou@mail.utoronto.ca>
2017-11-23 18:34:07 -0500

    Added field tagSet, as well as getter and setter for that field in class Tag.

--- phase2/src/Model/Tag.java
+++ phase2/src/Model/Tag.java
@@ -1,8 +1,11 @@
 package Model;
 
+import java.util.*;
+
 public class Tag {
 
     private final String content;
+    private static Set<Tag> tagSet;
 
     public Tag(String content) {
         this.content = content;
@@ -10,6 +13,14 @@ public class Tag {
 
     public String getContent() {
         return content;
+    }
+
+    public Set getTagSet(){
+        return tagSet;
+    }
+
+    public void setTagSet(Set<Tag> newTagSet){
+        Tag.tagSet = newTagSet;
     }
 
     @Override
