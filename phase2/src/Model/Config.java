d632b1bd4934b9dba8aa56ea494294eefa0ce23c
Sitao Wang <sitaowang1998@outlook.com>
2017-11-29 13:57:39 -0500

    Completed the javadoc for the config

--- phase2/src/Model/Config.java
+++ phase2/src/Model/Config.java
@@ -10,13 +10,26 @@ import java.io.FileReader;
 
 public class Config {
 
+    /* The root directory path. */
     private static String defaultPath = "";
+    /* The path of the config file. */
     private static final File configFile = new File(System.getProperty("user.dir"), "config.txt");
 
+    /**
+     * Returns true if the config file is in the project directory, false otherwise.
+     *
+     * @return whether the config file is in the project directory
+     */
     public static boolean hasConfigFile() {
         return configFile.isFile();
     }
 
+    /**
+     * Reads the config file to get the root directory path.
+     * Behaviour is undefine if the config file is empty or the first line is not a valid directory path
+     *
+     * @throws IOException if the config file is empty
+     */
     public static void readConfigFile() throws IOException {
         BufferedReader configBR = new BufferedReader(new FileReader(configFile));
 
@@ -29,15 +42,27 @@ public class Config {
         configBR.close();
     }
 
+    /**
+     * Deletes the config file if there is one.
+     */
     public static void deleteConfigFile() {
         configFile.delete();
     }
 
+    /**
+     * Creates the config file.
+     *
+     * @throws IOException if there is an existing config file
+     */
     public static void createConfigFile() throws IOException {
         configFile.createNewFile();
     }
 
-
+    /**
+     * Writes the root directory path to the file.
+     *
+     * @throws IOException if config file does not exist
+     */
     public static void writeConfigFile() throws IOException {
         FileOutputStream configFOS = new FileOutputStream(configFile);
         BufferedWriter configBW = new BufferedWriter(new OutputStreamWriter(configFOS));
@@ -48,10 +73,20 @@ public class Config {
         configFOS.close();
     }
 
+    /**
+     * Sets root directory to the input path.
+     *
+     * @param defaultPath the path root directory to be set to
+     */
     public static void setDefaultPath(String defaultPath) {
         Config.defaultPath = defaultPath;
     }
 
+    /**
+     * Returns the root directory.
+     * 
+     * @return the root directory
+     */
     public static String getDefaultPath() {
         return defaultPath;
     }
