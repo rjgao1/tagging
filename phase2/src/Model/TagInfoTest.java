744d3991164d1ac8fe0f92b92f15c61d5b2e2e8c
Jingyuan Gao <rooney.gao@mail.utoronto.ca>
2017-11-30 00:52:29 -0500

    Added TagInfoTest.java. Implemented TagInfoTest.setUp, dearDown, testTagInfoGetTime, and testToStringWithTags methods

---
+++ phase2/src/Model/TagInfoTest.java
@@ -0,0 +1,73 @@
+package Model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.io.IOException;
+import java.util.*;
+import java.io.*;
+
+class TagInfoTest {
+    private Tag tag1 = new Tag("tag1");
+    private Tag tag2 = new Tag("tag2");
+    private Tag tag3 = new Tag("tag3");
+    private Tag tag4 = new Tag("tag4");
+
+    private Tag[] tagList = new Tag[] {tag1, tag2, tag3, tag4};
+    private TagInfo tagInfo;
+
+    @BeforeEach
+    void setUp() {
+        this.tagInfo = new TagInfo(tagList);
+    }
+
+    @AfterEach
+    void tearDown() {
+        this.tagInfo = null;
+    }
+
+    @Test
+    public void testTagInfoGetTime() {
+//        Tag[] tagList = {new Tag("tag1"), new Tag("tag2"), new Tag("tag3"), new Tag("tag4")};
+//        TagInfo tagInfo = new TagInfo(tagList);
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date dateObj = new Date();
+
+        assertEquals(dateFormat.format(dateObj), tagInfo.getTime());
+    }
+
+
+
+    @org.junit.jupiter.api.Test
+    void getTagList() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTagListString() {
+    }
+
+    @Test
+    void testToStringWithTags() {
+        Tag[] tagList = {new Tag("tag1"), new Tag("tag2"), new Tag("tag3"), new Tag("tag4")};
+        TagInfo tagInfo = new TagInfo(tagList);
+        assertEquals(tagInfo.getTime()+"|@tag1@tag2@tag3@tag4", tagInfo.toString());
+    }
+
+
+
+    @org.junit.jupiter.api.Test
+    void stringToTagInfo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setTagListString() {
+    }
+
+
+
+}
